d1b07cb857fb84ec01138c9019eb6e8ad51ad629
akshay <akshay.khobragade7875@gmail.com>
2017-08-22 15:28:05 +0530

    7th commit, data bindind each food box after click shows values in card

---
+++ src/app/shared/dishes.ts
@@ -0,0 +1,28 @@
+import { Dish } from './dish';
+
+export const DISHES: Dish[] = [
+    {
+        name: 'Uthappizza',
+        image: '/assets/images/uthappizza.png',
+        category: 'mains',
+        label: 'Hot',
+        price: '4.99',
+        description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+    },
+    {
+        name: 'Zucchipakoda',
+        image: '/assets/images/zucchipakoda.png',
+        category: 'appetizer',
+        label: '',
+        price: '1.99',
+        description: 'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
+    },
+    {
+        name: 'ElaiCheese Cake',
+        image: '/assets/images/elaicheesecake.png',
+        category: 'dessert',
+        label: '',
+        price: '2.99',
+        description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
+    }
+];
